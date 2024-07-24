f2c34be7f6f80a2497c3be85298b525f01287cf0
joeriddles <joeriddles10@gmail.com>
2024-07-24 09:22:05 -0700

    Add automapper

---
+++ pkg/automapper/automapper.go
@@ -0,0 +1,81 @@
+package automapper
+
+import (
+	"encoding/json"
+	"reflect"
+)
+
+type AutoMapper[F any, T any] interface {
+	MapTo(from *F, to *T) error
+	MapFrom(to *T, from *F) error
+}
+
+func NewAutoMapper[F any, T any](from F, to T) AutoMapper[F, T] {
+	return &autoMapper[F, T]{}
+}
+
+type autoMapper[F any, T any] struct {
+}
+
+func (m *autoMapper[F, T]) MapTo(src *F, dst *T) error {
+	return m.doMap(src, dst)
+
+}
+
+func (m *autoMapper[F, T]) MapFrom(src *T, dst *F) error {
+	return m.doMap(src, dst)
+}
+
+func (m *autoMapper[F, T]) doMap(src any, dst any) error {
+	srcValue := reflect.ValueOf(src)
+	dstValue := reflect.ValueOf(dst)
+
+	if srcValue.Kind() == reflect.Pointer {
+		srcValue = srcValue.Elem()
+	}
+	if dstValue.Kind() == reflect.Pointer {
+		dstValue = dstValue.Elem()
+	}
+
+	if srcValue.Kind() == reflect.Struct && dstValue.Kind() == reflect.Struct {
+		m.mapStruct(src, dst)
+	} else if srcValue.CanConvert(dstValue.Type()) {
+		srcJson, _ := json.Marshal(src)
+		_ = json.Unmarshal(srcJson, dst)
+	}
+	return nil
+}
+
+func (m *autoMapper[F, T]) mapStruct(src any, dst any) {
+	srcVal := reflect.ValueOf(src).Elem()
+	dstVal := reflect.ValueOf(dst).Elem()
+
+	for i := 0; i < srcVal.NumField(); i++ {
+		srcField := srcVal.Field(i)
+		srcFieldType := srcVal.Type().Field(i)
+
+		dstField := dstVal.FieldByName(srcFieldType.Name)
+		if dstField.Kind() == reflect.Invalid {
+			// If the names of the fields are different between the from and to types,
+			// rely on the ordering of the fields instead
+			dstField = dstVal.Field(i)
+		}
+
+		// if srcField.Kind() == reflect.Struct && dstField.Kind() == reflect.Struct {
+		// 	srcFieldVal := srcField.Interface()
+		// 	dstFieldVal := dstField.Interface()
+		// 	fieldMapper := CreateAutoMapper(srcFieldVal, dstFieldVal)
+		// 	fieldMapper.MapTo(&srcFieldVal, &dstFieldVal)
+		// 	continue
+		// }
+
+		if dstField.IsValid() && dstField.CanSet() {
+			// Convert to the correct type, i.e. if src is int and dst is int64
+			if srcField.CanConvert(dstField.Type()) {
+				srcField = srcField.Convert(dstField.Type())
+			}
+
+			dstField.Set(srcField)
+		}
+	}
+}
---
+++ pkg/automapper/automapper_test.go
@@ -0,0 +1,185 @@
+package automapper
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAutoMapper_int(t *testing.T) {
+	// Arrange
+	mapper := NewAutoMapper(0, 0)
+	from := 1
+	to := -1
+
+	// Assert
+	err := mapper.MapTo(&from, &to)
+
+	// Assert
+	require.NoError(t, err)
+	assert.Equal(t, 1, to)
+}
+
+type User struct {
+	Name string
+}
+
+type User2 struct {
+	Name string
+}
+
+func TestAutoMapper_Struct(t *testing.T) {
+	// Arrange
+	mapper := NewAutoMapper(User{}, User2{})
+	from := User{Name: "Bob"}
+	to := User2{}
+
+	// Act
+	err := mapper.MapTo(&from, &to)
+
+	// Assert
+	require.NoError(t, err)
+
+	expected := User2{Name: "Bob"}
+	assert.Equal(t, expected, to)
+	assertJsonEq(t, expected, to)
+}
+
+type Cart struct {
+	Items []string
+}
+
+type Cart2 struct {
+	Items []string
+}
+
+func TestAutoMapper_StructWithList(t *testing.T) {
+	// Arrange
+	mapper := NewAutoMapper(Cart{}, Cart2{})
+	from := Cart{Items: []string{"apple", "orange"}}
+	to := Cart2{}
+
+	// Act
+	err := mapper.MapTo(&from, &to)
+
+	// Assert
+	require.NoError(t, err)
+
+	expected := Cart2{Items: []string{"apple", "orange"}}
+	assert.Equal(t, expected, to)
+	assertJsonEq(t, expected, to)
+}
+
+type DifferentCase struct {
+	UserId int
+}
+
+type DifferentCase2 struct {
+	UserID int
+}
+
+func TestAutoMapper_StructDifferentCase(t *testing.T) {
+	// Arrange
+	mapper := NewAutoMapper(DifferentCase{}, DifferentCase2{})
+	from := DifferentCase{UserId: 1}
+	to := DifferentCase2{}
+
+	// Act
+	err := mapper.MapTo(&from, &to)
+
+	// Assert
+	require.NoError(t, err)
+
+	expected := DifferentCase2{UserID: 1}
+	assert.Equal(t, expected, to)
+	assertJsonEq(t, expected, to)
+}
+
+type DifferentType struct {
+	UserId int
+}
+
+type DifferentType2 struct {
+	UserId int64
+}
+
+func TestAutoMapper_StructDifferentType(t *testing.T) {
+	// Arrange
+	mapper := NewAutoMapper(DifferentType{}, DifferentType2{})
+	from := DifferentType{UserId: 1}
+	to := DifferentType2{}
+
+	// Act
+	err := mapper.MapTo(&from, &to)
+
+	// Assert
+	require.NoError(t, err)
+
+	expected := DifferentType2{UserId: 1}
+	assert.Equal(t, expected, to)
+	assertJsonEq(t, expected, to)
+}
+
+type NestedType struct {
+	User User
+}
+
+type NestedType2 struct {
+	User User2
+}
+
+func TestAutoMapper_StructNestedType(t *testing.T) {
+	// Arrange
+	mapper := NewAutoMapper(NestedType{}, NestedType2{})
+	from := NestedType{User: User{Name: "Bob"}}
+	to := NestedType2{}
+
+	// Act
+	err := mapper.MapTo(&from, &to)
+
+	// Assert
+	require.NoError(t, err)
+
+	expected := NestedType2{User: User2{Name: "Bob"}}
+	assert.Equal(t, expected, to)
+	assertJsonEq(t, expected, to)
+}
+
+type NestedTypePointer struct {
+	User *User
+}
+
+type NestedTypePointer2 struct {
+	User *User2
+}
+
+func TestAutoMapper_StructNestedPointerType(t *testing.T) {
+	// Arrange
+	mapper := NewAutoMapper(NestedTypePointer{}, NestedTypePointer2{})
+	from := NestedTypePointer{User: &User{Name: "Bob"}}
+	to := NestedTypePointer2{}
+
+	// Act
+	err := mapper.MapTo(&from, &to)
+
+	// Assert
+	require.NoError(t, err)
+
+	expected := NestedTypePointer2{User: &User2{Name: "Bob"}}
+	assert.Equal(t, expected, to)
+	assertJsonEq(t, expected, to)
+}
+
+func assertJsonEq(t *testing.T, expected any, actual any) {
+	actualBytes, err := json.Marshal(actual)
+	require.NoError(t, err)
+	actualJson := string(actualBytes)
+
+	expectedBytes, err := json.Marshal(expected)
+	require.NoError(t, err)
+	expectedJson := string(expectedBytes)
+
+	assert.JSONEq(t, expectedJson, actualJson)
+}
